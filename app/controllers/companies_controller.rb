c613fc470d79a2a4b8142964fbdacf5e9628f938
chaniel <netqyq@gmail.com>
2016-03-15 18:28:09 +0800

    at the end of 06_06, how to  rendering json xml format, some style, turbolinks, helper ...

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,5 +8,12 @@ class CompaniesController < ApplicationController
 #    @hello = "hello world!"
 
     @company = Company.find(params[:id])    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render xml: @company }
+      format.json { render json: @company }
+    end
+
+
   end
 end
