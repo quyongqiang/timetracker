54330556176ac615e705c144a6295b708d812e83
chaniel <netqyq@gmail.com>
2016-04-10 16:21:39 +0800

    end of chapter11 testing, test failure, review in file test/models/work_test.rb

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CompaniesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  test "the truth" do
+    assert true
+  end
+  
+  # 
+#   test "should get index" do
+#     get :index
+#     assert_response :success
+#   end
+#  
+#  
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
   # end
+
+
 end
